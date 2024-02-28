87686b6950c4e7a9a6ed5010a078c43c279ddd97
kalpesh <manandhar.kalpesh@gmail.com>
2024-02-28 13:39:18 +0545

    visualizing himmelblau with intermediate solutions
    the solver gives a list of solution states at given iterations if given
    plots the given solution states

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from ABC_solver import ABC_solver;
 from math import exp;
 
 import numpy as np;
+import matplotlib.pyplot as plt
 
 
 def Himmelblau_Function(x,y):
@@ -23,7 +24,9 @@ def main():
     N_SOURCES = 50
     N_PARAMS = 2
     MAX_ITERATIONS = 1000
-    SOLUTION_RANGE = (-50,50)
+    SOLUTION_RANGE = (-6,6)
+
+    INTERMEDIATES = [0,50,100,250,500,700,950]
     
     
     abc = ABC_solver(LIMIT, N_SOURCES, N_PARAMS, 
@@ -32,7 +35,41 @@ def main():
 
     abc.setFitFunction(himmelblau_fit)
 
-    print(abc.solve())
+    (solution, intermediates)= abc.solve(INTERMEDIATES)
+    print(solution)
+
+
+
+    # plot
+
+    X= np.linspace(-6,6)    # returns an array of evenly spaced values with in the specified interval
+    Y= np.linspace(-6,6)
+
+    x,y = np.meshgrid(X,Y)   # convert the 1D vectors representing the axes into 2D arrays.
+    F = Himmelblau_Function(x,y)
+
+    ax = plt.axes(projection = '3d')
+    ax.contour3D(x, y, F, 450)
+
+    for slns in intermediates:
+        xy = np.transpose(slns)
+        xs = xy[0]
+        ys = xy[1]
+        zs = Himmelblau_Function(xs,ys)
+        ax.scatter(xs,ys,zs, marker='o', color= "red")
+
+    ax.scatter([solution[0]], [solution[1]], 
+               [Himmelblau_Function(solution[0], solution[1])], 
+               marker='^', color= "green")
+
+    ax.set_xlabel('X')
+    ax.set_xlabel('Y')
+    ax.set_zlabel('F')
+    ax.set_title('Himmelblau Function')
+    ax.view_init(50, 50)
+
+    plt.show()
+
 
 
 if (__name__ == "__main__"):
